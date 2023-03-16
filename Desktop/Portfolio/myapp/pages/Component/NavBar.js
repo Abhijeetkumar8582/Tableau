ef9957ecc311d065c23c2e497796d12235597846
Abhijeet kumar <abhijeet122kumar@gmail.com>
2023-03-16 23:42:18 +0530

    forntPage

--- Desktop/Portfolio/myapp/pages/Component/NavBar.js
+++ Desktop/Portfolio/myapp/pages/Component/NavBar.js
@@ -2,31 +2,27 @@
 import Link from 'next/link'
 
 function NavBar() {
+
     return (
         <>
-
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "#FDA260" }}>
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="#">Navbar</Link>
+                    <Link className="navbar-brand" href="#" style={{ color: "white" }}>Abhi</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse  justify-content-center" id="navbarSupportedContent" >
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item">
-                                <Link class="nav-link active" href="#">About</Link>
+                        <ul className="nav justify-content-center ">
+                            <li className="nav-item">
+                                <Link className="nav-link active mx-2" href="#">About</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="/Product/product">Expirence</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-2" href="/Product/product">Expirence</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="#">Projects</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link mx-2" href="#">Projects</Link>
                             </li>
                         </ul>
-
                     </div>
                 </div>
             </nav>
--- Desktop/Portfolio/myapp/pages/index.js
+++ Desktop/Portfolio/myapp/pages/index.js
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import Script from 'next/Script'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import NavBar from './Component/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,14 +7,8 @@ export default function Home() {
   return (
     <>
       <NavBar/>
-      <button type="button" class="btn btn-primary my-5 mx-4">Primary</button>
-      <button type="button" class="btn btn-secondary mx-4">Secondary</button>
-      <button type="button" class="btn btn-success mx-4">Success</button>
-      <button type="button" class="btn btn-danger mx-4">Danger</button>
-      <button type="button" class="btn btn-warning mx-4">Warning</button>
-      <button type="button" class="btn btn-info mx-4">Info</button>
-      <button type="button" class="btn btn-light mx-4">Light</button>
-      <button type="button" class="btn btn-dark mx-4">Dark</button>
+      <h1 className='text-center' style={{ marginTop:"10%",fontFamily:"Copperplate" }}><strong>Forntend Developer & ChatBot Developer</strong></h1>
+      <p className='text-center my-4 mx-4' style={{fontFamily:"Chalkduster"}}>React-ing like there's no tomorrow: How I learned to stop worrying and love the component lifecycle!</p>
     </>
   )
 }
