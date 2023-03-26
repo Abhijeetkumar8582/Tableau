714fdb5ae113fb66afd70f313e3496e58dfab796
Abhijeet kumar <abhijeet122kumar@gmail.com>
2023-03-26 16:10:28 +0530

    first commit

---
+++ Desktop/Brownmen/myblog/pages/_app.js
@@ -0,0 +1,11 @@
+import '@/styles/globals.css'
+import Footer from './Component/Footer'
+import Navbar from './Component/Navbar'
+
+export default function App({ Component, pageProps }) {
+  return <>
+<Navbar/>
+<Component {...pageProps} />
+<Footer/>
+  </>
+}
